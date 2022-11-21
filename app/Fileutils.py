c778b1393f4ab9c10c4dc73a2d7e0b6e7eb199ea
sayanmondal2098 <sayanmondal2098@gmail.com>
2022-11-22 01:13:36 +0530

    clean up fileutils

--- app/Fileutils.py
+++ app/Fileutils.py
@@ -1,8 +1,5 @@
 import shutil
 
-
-shutil.make_archive('/home/user/Desktop/Filename','zip','/home/username/Desktop/Directory')
-
 #  res = archive('INC000001','./downloads/')
 def archive(filename, directory):
     shutil.make_archive( base_name = './zipfile/'+filename, format= 'zip' , base_dir= directory)
